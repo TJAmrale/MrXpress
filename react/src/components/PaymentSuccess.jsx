b179ca9e1a1330e817970d832c44dda35c3bb450
andrewnguyen-nsw <andrewnguyen.nsw@gmail.com>
2023-10-23 13:33:57 +1100

    Update payment, update .env.example, package.json

--- react/src/components/PaymentSuccess.jsx
+++ react/src/components/PaymentSuccess.jsx
@@ -1,10 +1,23 @@
 /* eslint-disable react/prop-types */
-import { Card, Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Button, Card, Container } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 function PaymentSuccess({ jobId }) {
+  const [bookingInformation, setBookingInformation] = useState({});
+  const [showBookingInfo, setShowBookingInfo] = useState(false); // State to toggle booking information visibility
+
+  useEffect(() => {
+    const savedBookingInfo = localStorage.getItem("bookingInformation");
+    if (savedBookingInfo) {
+      setBookingInformation(JSON.parse(savedBookingInfo));
+    }
+  }, []);
+
+  console.log(bookingInformation); // this print undefined
+
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
@@ -24,17 +37,50 @@ function PaymentSuccess({ jobId }) {
           </Card.Title>
           <Card.Text className="text-center">Booking ID: #{jobId}</Card.Text>
           <Card.Text className="text-center">
-            Thank you for choosing us among <br></br>many options available! Your
-            trust is <br></br>immensely valued.
+            Thank you for choosing us among many options available!
+            Your trust is immensely valued.
           </Card.Text>
           <Card.Text className="text-center">
             A skilled technician will be in touch with you shortly to assist
             with your phone repair needs.
           </Card.Text>
-          <Card.Text className="text-center">
-            <Link to="/app">Back to home</Link>
-          </Card.Text>
+          <div className="row">
+            {" "}
+            {/* Create a row */}
+            <div className="col-6">
+              {" "}
+              <Link to="/app" className="w-100">
+                <Button variant="outline-primary" className="w-100">
+                  Back to Home
+                </Button>
+              </Link>
+            </div>
+            <div className="col-6">
+              {" "}
+              <Button
+                variant="primary"
+                onClick={() => setShowBookingInfo(!showBookingInfo)}
+                className="w-100"
+              >
+                {showBookingInfo
+                  ? "Hide Booking Info"
+                  : "Show Booking Info"}
+              </Button>
+            </div>
+          </div>
         </Card.Body>
+        {showBookingInfo && ( 
+          <div className="booking-information-receipt font-monospace">
+            <p>Device: {bookingInformation.device}</p>
+            <p>Repair Type: {bookingInformation.repairType}</p>
+            {bookingInformation.accessories && (
+              <p>Accessories: {bookingInformation.accessories}</p>
+            )}
+            <p>Total Cost: ${bookingInformation.totalCost}</p>
+            <p>Phone Number: {bookingInformation.phoneNumber}</p>
+            <p>Address: {bookingInformation.address}</p>
+          </div>
+        )}
       </Card>
     </Container>
   );
